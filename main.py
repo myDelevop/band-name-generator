72b7a05bb060d3992401571c8df2188c90b2fa56
Rocco Caliandro <rocco@toptal.com>
2023-08-17 13:04:10 +0200

    input function and comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Write your code below this line 👇
 print("Hello World!")
 print("Hello World!\nHello World!")
-print("Hello " + "Rocco")
-# print("Hello" + " Rocco")
-# print("Hello " + " " + "Rocco")
+print("Hello " + "Rocco") # print("Hello" + " Rocco") # print("Hello " + " " + "Rocco")
+
+# input("What is your name?")
+print("Hello " + input("What is your name?"))

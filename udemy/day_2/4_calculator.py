ee972c73f92679edacc1ccab65fa088f5d9722c8
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-26 19:39:23 +0200

    added calculator file

--- udemy/day_2/4_calculator.py
+++ udemy/day_2/4_calculator.py
@@ -2,7 +2,6 @@ print("Welcome to the tip calculator!")
 
 
 total_bill = input("What was the total bill? $")
-
 perc_tip = input("How much would you like to give? 10, 12 or 15? ")
 num_people = input("How many people to split the bill?")
 

7bb88045a3bcbae75bb9a3dc4475e75b33114728
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-26 19:43:48 +0200

    num_people casted as int

--- udemy/day_2/4_calculator.py
+++ udemy/day_2/4_calculator.py
@@ -6,6 +6,6 @@ perc_tip = input("How much would you like to give? 10, 12 or 15? ")
 num_people = input("How many people to split the bill?")
 
 
-total = round((float(total_bill) * (int(perc_tip)/100) + float(total_bill)) / float(num_people), 2)
+total = round((float(total_bill) * (int(perc_tip)/100) + float(total_bill)) / int(num_people), 2)
 
 print(f"Each person should pay: ${total}")

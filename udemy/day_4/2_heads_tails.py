e1f7ff7395334a2db107eb1b63e89a9d75bf349e
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-27 16:35:54 +0200

    created personal python module

---
+++ udemy/day_4/2_heads_tails.py
@@ -0,0 +1,14 @@
+import random
+
+# import my_module
+# print(my_module.pi) Not in this exercise
+
+random_number = random.randint(0, 1)
+#print(random_number)
+
+if random_number == 1:
+    print("Heads")
+elif random_number == 0:
+    print("Tails")
+else:
+    print("Error!")

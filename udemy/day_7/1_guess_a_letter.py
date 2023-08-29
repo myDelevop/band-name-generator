5e1a1ce36c83b38535db5e7f191806abc55eec19
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-29 12:03:12 +0200

    Changed variable names according requirements

--- udemy/day_7/1_guess_a_letter.py
+++ udemy/day_7/1_guess_a_letter.py
@@ -4,7 +4,7 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = word_list[random.randint(0, 2)]
+chosen_word = random.choice(word_list)  # word_list[random.randint(0, 2)]
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()

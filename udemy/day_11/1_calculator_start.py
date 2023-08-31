a2213af565db9f2eb75c1db156239f46dc7d324c
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-31 17:37:15 +0200

    Calculator Start basic operations

---
+++ udemy/day_11/1_calculator_start.py
@@ -0,0 +1,36 @@
+from art import logo
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 -n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        print("It's impossible to divide by 0")
+        return
+    return n1 / n2
+
+print(logo)
+
+dictionary = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for key in dictionary:
+    print(key)
+
+operation = input("Pick an operation from the line above: ")
+if dictionary.__contains__(operation):
+    answer = dictionary[operation](num1, num2)
+    print(f"{num1} {operation} {num2} = {answer}")

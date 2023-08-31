e6ad0fbd16c54f296b83aa8a02772af6054a7056
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-08-31 15:19:21 +0200

    Added return statement if first or second parameter is null

--- udemy/day_10/1_function_with_output.py
+++ udemy/day_10/1_function_with_output.py
@@ -1,8 +1,11 @@
 def formate_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
 
-    return f"{formatted_f_name} {formatted_l_name}"
+    return f"Result: {formatted_f_name} {formatted_l_name}"
 
 
 formatted_string = formate_name("roCco", "caLIandRO")

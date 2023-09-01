6fd9f012dcf8f45aff6490323bc75b49cac8413e
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-01 12:07:54 +0200

    file for constants and globals

---
+++ udemy/day_12/4_constants_and_globals.py
@@ -0,0 +1,16 @@
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@rcaliandro"
+
+# Constants are written in UPPER case, and they are not supposed to be changed
+
+
+def calc():
+    output = PI * 5.669
+    print(URL + TWITTER_HANDLE)
+    return output
+
+
+print(calc())
+
+

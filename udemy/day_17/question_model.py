8b646e74f46b40bf74eface2afd6d0ac398c42cf
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-02 17:06:38 +0200

    Created DB of questions for our game

---
+++ udemy/day_17/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, question: str, answer: bool):
+        self.question = question
+        self.answer = answer
+
+
+# new_q = Question("2+5=7", "True")
+# print(new_q.question + " " + str(new_q.answer))

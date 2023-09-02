6967c1977c27e5124b70aaf3bc8c36f4e161852c
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-02 12:01:13 +0200

    Bug fixing

--- udemy/day_15/main.py
+++ udemy/day_15/main.py
@@ -15,6 +15,11 @@ def compute_total(n_quarters, n_dimes, n_nickles, n_pennies):
     return n_quarters * 0.25 + n_dimes * 0.1 + n_nickles * 0.05 + n_pennies * 0.01
 
 
+def update_resources(bought_drink):
+    for ingredient in MENU[bought_drink]["ingredients"]:
+        resources[ingredient] -= MENU[bought_drink]["ingredients"][ingredient]
+
+
 profit = 0
 
 quarters = 0
@@ -38,6 +43,7 @@ while is_on:
         if len(check_enough_resources(choice)) > 0:
             print(f"Sorry there is not enough {choice}.")
         else:  # we are able to do your drink
+            print("Please insert coin.")
             quarters = float(input("how many quarters?: "))
             dimes = float(input("how many dimes?: "))
             nickles = float(input("how many nickles?: "))
@@ -46,13 +52,15 @@ while is_on:
             user_money = compute_total(quarters, dimes, nickles, pennies)
             drink_price = MENU[choice]['cost']
 
-            if round(drink_price, 2) > round(user_money, 2):
+            if (drink_price, 2) > (user_money, 2):
                 print("Sorry that's not enough money. Money refunded.")
             elif round(drink_price, 2) == round(user_money, 2):
                 profit += drink_price
             elif round(drink_price, 2) < round(user_money, 2):
                 change = round(user_money, 2) - round(drink_price,2)
-                profit += round(drink_price)
+                profit += round(drink_price, 2)
+
+                update_resources(choice)
                 print(f"Here is ${round(change,2)} dollars in change.")
 
-                print(f"Here is your {choice}. Enjoy!")
+                print(f"Here is your {choice} ☕. Enjoy!")

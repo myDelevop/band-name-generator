91db2b9acef1022eb29dc0940a48d4502e90879a
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-03 15:31:48 +0200

    Moving food randomly when eaten

--- udemy/day_21/main.py
+++ udemy/day_21/main.py
@@ -25,7 +25,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collection with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()

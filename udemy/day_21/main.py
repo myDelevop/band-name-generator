4857042feee8f0d90302bb7424ae18dc37e08d40
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-03 17:29:26 +0200

    game over in case of collision with the snake tail

--- udemy/day_21/main.py
+++ udemy/day_21/main.py
@@ -38,7 +38,15 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
 
-    # Detect snake collision
+    # Detect snake collision with tail
+    # if the head collides wth any segments in the tail, game over
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+
+
 
 scoreboard.game_over()
 

cb8f6f0fe9461e697c7e070936fdda1a8e790f13
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-04 11:37:38 +0200

    Aligned Level to the left

--- udemy/day_23/scoreboard.py
+++ udemy/day_23/scoreboard.py
@@ -9,14 +9,15 @@ class ScoreBoard(Turtle):
         self.color("black")
         self.penup()
         self.hideturtle()
+        self.goto(-280, 250)
         self.level = 1
         self.update_level()
 
     def update_level(self):
         self.clear()
-        self.goto(0, 250)
-        self.write(f"Level: {self.level}", align="center", font=FONT)
+        self.goto(-280, 250)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
-        self.write("Game Over!", align="center", font=FONT)
+        self.write("Game Over!", align="left", font=FONT)

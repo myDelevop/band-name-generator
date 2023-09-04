03372a83b3f6540f50fe50654b31cc12ab19fcf9
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-04 18:49:57 +0200

    manipulating data by using pandas

--- udemy/day_25/main.py
+++ udemy/day_25/main.py
@@ -20,5 +20,52 @@ import csv
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+
+print(data.to_dict())
+temp_list = data["temp"].tolist()
+print(temp_list)
+
+total = sum(temp_list)
+avg_tmp = round(float(total / len(temp_list)), 2)
+
+print(f"AVG: {avg_tmp}")
+
+# By using pandas
+print(f"AVG: {round(data['temp'].mean(), 2)}")
+print(f"Max: {data['temp'].max()}")
+
+# Get Data in Columns
+print(data["condition"])
+print(data.condition)
+
+# Get data in a specific row
+print(data[data.day == "Monday"])
+# Output: "0  Monday    12     Sunny"
+
+print("**************************")
+#  Get data in the row with max temp
+maximum = data.temp.max()
+print(data[data.temp == maximum])
+print("**************************")
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+# monday's temperature in Faranait not Celcius
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0]
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
+
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
 print(data)
-print(data["temp"])
+data.to_csv("new_data.csv")

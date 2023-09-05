8e39045218c52178d482b29171e0fde15482d784
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-05 10:36:23 +0200

    changed turtle speed and removed score attribute (replaced with len(guessed)

--- udemy/day_25/3_states_game.py
+++ udemy/day_25/3_states_game.py
@@ -4,6 +4,7 @@ import turtle
 
 def get_mouse_click_corner(x, y):
     turtle.pu()
+    turtle.speed(1)
     turtle.goto(x, y)
 
 
@@ -36,18 +37,16 @@ screen.setup(width=800, height=600, startx=-260, starty=20)
 
 turtle.onscreenclick(get_mouse_click_corner)
 
-is_game = True
-guessed = []
-score = 0
 
-while is_game:
-    if score > 0:
-        textinput = screen.textinput(title=f"{score}/{50} States Correct", prompt="What's another state name?")
+guessed = []
+
+while len(guessed) < 50:
+    if len(guessed) > 0:
+        textinput = screen.textinput(title=f"{len(guessed)}/{50} States Correct", prompt="What's another state name?")
     else:
         textinput = screen.textinput(title="Guess the State", prompt="What's another state name?")
     check = next((item for item in states if str(item["State"]).lower() == textinput.lower()), None)
     if check is not None and check["State"].lower() not in guessed:
-        score += 1
         guessed.append(str(check["State"]).lower())
         insert_state_in_map(check["State"], check["position"])
         get_mouse_click_corner(check["position"][0], check["position"][1])

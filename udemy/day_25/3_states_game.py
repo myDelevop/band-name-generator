dcd64705843d3848425e9f398c31dab4ff1739e0
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-05 10:58:14 +0200

    generated a new file with the states not guessed by the user

--- udemy/day_25/3_states_game.py
+++ udemy/day_25/3_states_game.py
@@ -34,9 +34,7 @@ screen.title("U.S. States Game")
 screen.bgpic("blank_states_img.gif")
 screen.setup(width=800, height=600, startx=-260, starty=20)
 
-
 turtle.onscreenclick(get_mouse_click_corner)
-
 
 guessed = []
 
@@ -45,10 +43,20 @@ while len(guessed) < 50:
         textinput = screen.textinput(title=f"{len(guessed)}/{50} States Correct", prompt="What's another state name?")
     else:
         textinput = screen.textinput(title="Guess the State", prompt="What's another state name?")
+    if textinput.lower() == "exit":
+        break
     check = next((item for item in states if str(item["State"]).lower() == textinput.lower()), None)
     if check is not None and check["State"].lower() not in guessed:
         guessed.append(str(check["State"]).lower())
         insert_state_in_map(check["State"], check["position"])
         get_mouse_click_corner(check["position"][0], check["position"][1])
 
-turtle.mainloop()
+# The game is finished, we generate a file with the not guessed Countries
+with open("states_to_learn.csv", "w") as missed_csv:
+    missed_csv.write("state,x,y\n")
+    for obj in states:
+        if str(obj["State"]).lower() not in guessed:
+            missed_csv.write(f"{obj['State']}, {obj['position'][0]}, {obj['position'][1]}\n")
+
+# turtle.mainloop()
+

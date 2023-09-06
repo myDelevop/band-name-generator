22086593ae577bdfe0d4d6fe8625f0e67bf82d3a
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-06 11:03:45 +0200

    closed open files

--- udemy/day_26/4_data_overlap.py
+++ udemy/day_26/4_data_overlap.py
@@ -4,6 +4,9 @@ lines_1 = file_1.readlines()
 file_2 = open("file2.txt", "r")
 lines_2 = file_2.readlines()
 
+file_1.close()
+file_2.close()
+
 # Cleaning
 lines_1 = [int((line.strip())) for line in lines_1]
 lines_2 = [int((line.strip())) for line in lines_2]
@@ -15,5 +18,3 @@ result = [num for num in lines_1 if num in lines_2]
 # Write your code above 👆
 
 print(result)
-
-

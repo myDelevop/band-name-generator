6d98d482b049d26ad6acb6c39815924ba84369a0
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-06 16:49:14 +0200

    unlimited positional arguments

---
+++ udemy/day_27/2_many_positional_arguments.py
@@ -0,0 +1,10 @@
+# Unlimited POSITIONAL arguments (args[0) = 3, first argument)
+
+def add(*args):
+    res = 0
+    for n in args:
+        res += n
+    return res
+
+
+print(add(3, 4, 5, 2, 1, 7, 4, 3))

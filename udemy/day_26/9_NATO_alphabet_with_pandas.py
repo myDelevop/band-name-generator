6e977e786758d801d5093243f13340c8bb6efdc5
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-07 21:27:45 +0200

    Previous NATO project. User input until there is no IndexException

--- udemy/day_26/9_NATO_alphabet_with_pandas.py
+++ udemy/day_26/9_NATO_alphabet_with_pandas.py
@@ -3,9 +3,17 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
+print(phonetic_dict)
 
-user_input = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in user_input]
 
-print(output_list)
+valid_input = False
+
+while valid_input is False:
+    user_input = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letter in the alphabet, please.")
+    else:
+        valid_input = True
+        print(output_list)

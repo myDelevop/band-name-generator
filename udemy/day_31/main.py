1d37859ae183efd23faf7f2268585e47bc543cce
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 11:01:51 +0200

    show french cards

--- udemy/day_31/main.py
+++ udemy/day_31/main.py
@@ -1,6 +1,19 @@
+import pandas
+import random
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_tile, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    pass
+
 
 screen = Tk()
 screen.title("Flashy")
@@ -14,17 +27,18 @@ card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+card_tile = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 
+next_card()
 
 screen.mainloop()

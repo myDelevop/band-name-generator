9d24d065540661f0f4144c7fe14138951dd4d21e
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 12:50:37 +0200

    Send email using python

---
+++ udemy/day_32/1_send_email.py
@@ -0,0 +1,14 @@
+import smtplib
+
+my_email = "rockdesires@gmail.com"
+to_address = "rocco.caliandro@toptal.com"
+password = "rwzuaeheaajizvma"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=to_address,
+                        msg="Subject:Hello from Python\n\nThis is the body of my email"
+                        )

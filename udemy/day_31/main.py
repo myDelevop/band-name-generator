fbf6571b72aedfed4a7ed6868188e7af9d387adb
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 11:17:05 +0200

    insert flipped card logic to switch between english and french every 3 seconds

--- udemy/day_31/main.py
+++ udemy/day_31/main.py
@@ -6,13 +6,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    screen.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_tile, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
-    pass
+    canvas.itemconfig(card_tile, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = screen.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_tile, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 screen = Tk()
@@ -20,11 +30,13 @@ screen.title("Flashy")
 # screen.minsize(width=900, height=600)
 screen.configure(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = screen.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 
-card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_tile = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))

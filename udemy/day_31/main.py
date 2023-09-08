d19ea93baa5f369fc014a82603f4c77a4473f16a
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 11:56:15 +0200

    Created a file with the words to learn. If the file is empty we read from the original one

--- udemy/day_31/main.py
+++ udemy/day_31/main.py
@@ -4,9 +4,16 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+to_learn = {}
 current_card = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except (pandas.errors.EmptyDataError, FileNotFoundError):
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -17,6 +24,15 @@ def next_card():
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = screen.after(3000, func=flip_card)
+
+
+def is_known():
+    to_learn.remove(current_card)
+
+    data_csv = pandas.DataFrame(to_learn)
+    data_csv.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 
 def flip_card():
@@ -48,7 +64,7 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(row=1, column=1)
 
 next_card()

75512ca22525bc5ade27191b51f083d79cd35b2e
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 18:01:14 +0200

    updated birthdays.csv

--- udemy/day_32/4_auto_birthday_wisher.py
+++ udemy/day_32/4_auto_birthday_wisher.py
@@ -1,8 +1,11 @@
+import datetime as dt
 ##################### Extra Hard Starting Project ######################
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+with open("")
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 

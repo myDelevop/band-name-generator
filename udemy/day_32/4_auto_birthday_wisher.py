1e2556240c05006d45df9940ce6ba2bb78c2011d
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-08 18:38:47 +0200

    Wish a happy birthday based to CSV file

--- udemy/day_32/4_auto_birthday_wisher.py
+++ udemy/day_32/4_auto_birthday_wisher.py
@@ -1,16 +1,41 @@
+import random
+import smtplib
 import datetime as dt
-##################### Extra Hard Starting Project ######################
 
-# 1. Update the birthdays.csv
+MY_EMAIL = "rockdesires@gmail.com"
+MY_PSW = "rwzuaeheaajizvma"
 
-# 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
-with open("")
+month = int(now.month)
+day = int(now.day)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+today_birthdays = []
 
-# 4. Send the letter generated in step 3 to that person's email address.
+with open("birthdays.csv") as birthdays_file:
+    data = birthdays_file.readlines()
+    for line in data:
+        line = line.strip()
+        if not (line.startswith("name") or line == ""):
+            file_month = int(line.split(',')[3])
+            file_day = int(line.split(',')[4])
+
+            if file_month == month and file_day == day:
+                today_birthdays.append({
+                    "name": line.split(',')[0],
+                    "email": line.split(',')[1]
+                })
 
 
+for birthday in today_birthdays:
+    random_num = random.randint(1, 3)
 
+    with open(file="letter_templates/letter_" + str(random_num) + ".txt") as template:
+        text = template.read().replace("[NAME]", birthday["name"])
 
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PSW)
+
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday["email"],
+                            msg="Subject:Happy Birthday!\n\n" + text)

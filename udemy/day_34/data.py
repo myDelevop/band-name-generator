420eb775e1811aecc8f37b8f4284989ffa6c24fa
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-09 09:11:27 +0200

    set parameters in an object

--- udemy/day_34/data.py
+++ udemy/day_34/data.py
@@ -1,7 +1,12 @@
 import requests
 
-request = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
-request.raise_for_status()
-data = request.json()
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
 
 question_data = data["results"]

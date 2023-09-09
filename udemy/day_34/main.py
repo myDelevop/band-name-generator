016fea0c60f649aa4c3055e97361feb31a4e4adb
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-09 10:03:46 +0200

    Created a freezed GUI for dynamic Trivia questions

--- udemy/day_34/main.py
+++ udemy/day_34/main.py
@@ -1,6 +1,8 @@
+from ui import QuizInterface
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+
 
 question_bank = []
 for question in question_data:
@@ -11,9 +13,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- udemy/day_34/ui.py
+++ udemy/day_34/ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+        self.score = 0
+
+        self.score_label = Label(text=f"Score: {self.score}", font=("Arial", 10, "normal"), bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some Question Text",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        right_image = PhotoImage(file="images/true.png")
+        self.right_button = Button(image=right_image, highlightthickness=0)
+        self.right_button.grid(row=2, column=0)
+
+        wrong_image = PhotoImage(file="images/false.png")
+        self.wrong_button = Button(image=wrong_image, highlightthickness=0)
+        self.wrong_button.grid(row=2, column=1)
+
+        self.window.mainloop()

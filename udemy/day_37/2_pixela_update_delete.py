9aed097a336d8dbf2fdd8f24471bd7eb003863d1
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-09 20:05:20 +0200

    Delete a pixel given a date

--- udemy/day_37/2_pixela_update_delete.py
+++ udemy/day_37/2_pixela_update_delete.py
@@ -5,15 +5,23 @@ USERNAME = "myuser97"
 GRAPH_ID = "graph1"
 TOKEN = "neiwfni7622ufh8uhqwe"
 UPDATE_PIXEL_ENDPOINT = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}/20230908"
+DELETE_PIXEL_ENDPOINT = UPDATE_PIXEL_ENDPOINT
+
 
 token_header = {
     "X-USER-TOKEN": TOKEN
 }
 
-update_pixel_parameters = {
-    "quantity": "4.32"
-}
-
-response = requests.put(url=UPDATE_PIXEL_ENDPOINT, headers=token_header, json=update_pixel_parameters)
-response.raise_for_status()
-print(str(response.status_code) + " - " + response.text)
+# update_pixel_parameters = {
+#     "quantity": "4.32"
+# }
+#
+# response = requests.put(url=UPDATE_PIXEL_ENDPOINT, headers=token_header, json=update_pixel_parameters)
+# response.raise_for_status()
+# print(str(response.status_code) + " - " + response.text)
+# UPDATED YESTERDAY'S PIECE OF DATA
+#
+# response = requests.delete(url=DELETE_PIXEL_ENDPOINT, headers=token_header)
+# response.raise_for_status()
+# print(str(response.status_code) + " - " + response.text)
+# DELETED AND INSERTED AGAIN

33082cb249a694072b0b07e480bbd9fec368d8ec
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-10 10:07:27 +0200

    auth with Bearer

---
+++ udemy/day_38/3_add_token_for_security.py
@@ -0,0 +1,53 @@
+import requests
+from datetime import datetime
+
+NUTRITIONIX_USERNAME = "mawafi3528"
+NUTRITIONIX_PASSWORD = "MV9iic!in%A"
+NUTRITIONIX_BEARER_TOKEN = "JHADSOHUIUSADFIHOUAGHA"
+NUTRITIONIX_APPLICATION_ID = "d888e201"
+NUTRITIONIX_APPLICATION_KEY = "1a9d7a45b664e6719c10b073db79a331"
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+SHEETY_ENDPOINT = "https://api.sheety.co/550056cfdf9916e403de58f74910dc55/myWorkouts/workouts"
+
+nutritionix_header = {
+    "x-app-id": NUTRITIONIX_APPLICATION_ID,
+    "x-app-key": NUTRITIONIX_APPLICATION_KEY,
+    "x-remote-user-id": "0"
+}
+
+query = input("Tell me which exercise you did: ")
+
+nutritionix_parameters = {
+    "query": query,
+    "gender": "female",
+    "weight_kg": "67",
+    "height_cm": "167",
+    "age": "26"
+}
+
+response = requests.post(url=NUTRITIONIX_ENDPOINT,
+                         json=nutritionix_parameters,
+                         headers=nutritionix_header)
+response.raise_for_status()
+response_json = response.json()
+
+today = datetime.now()
+today_date = today.strftime("%d/%m/%Y")
+today_time = today.strftime("%H:%M:%S")
+
+sheety_headers = {"Authorization": "Bearer JHADSOHUIUSADFIHOUAGHA"}
+
+
+for exercise in response_json["exercises"]:
+    parameters = {
+        "workout": {
+            "date": today_date,
+            "time": today_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheeety_response = requests.post(SHEETY_ENDPOINT, headers=sheety_headers, json=parameters)
+    print(str(sheeety_response.status_code) + " - " + sheeety_response.text)

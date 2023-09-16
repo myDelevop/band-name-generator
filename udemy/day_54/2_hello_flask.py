0ce4d4960a7784070b6d2431850df1d4bc77a868
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-16 11:00:05 +0200

    Added bye endpoint

--- udemy/day_54/2_hello_flask.py
+++ udemy/day_54/2_hello_flask.py
@@ -8,6 +8,12 @@ def hello_world():
     return "Hello, World!"
 
 
+# go to http://127.0.0.1:5000/bye
+@app.route('/bye')
+def say_bye():
+    return "Bye"
+
+
 if __name__ == "__main__":
     # Here we run without terminal and without env variable. If the name of the method
     # is __main__, is a script, then we call app.run() that launches the server directly

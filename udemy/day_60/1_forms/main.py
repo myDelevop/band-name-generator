6932bc77da3bce5f2da1368c8704f16d4a21f244
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-17 20:36:32 +0200

    imported blog website project

---
+++ udemy/day_60/1_forms/main.py
@@ -0,0 +1,20 @@
+import random
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template(template_name_or_list="index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

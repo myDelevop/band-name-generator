ba095c4183aa8d0a73d898056709ec5c3522daec
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-17 19:11:19 +0200

    get blog from API

--- udemy/day_59/server.py
+++ udemy/day_59/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+BLOG_ENDPOINT = "https://api.npoint.io/eb6cd8a5d783f501ee7d"
+response = requests.get(BLOG_ENDPOINT)
+response.raise_for_status()
+all_blogs = response.json()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template(template_name_or_list="index.html", json_blogs=all_blogs)
 
 @app.route('/about')
 def about():

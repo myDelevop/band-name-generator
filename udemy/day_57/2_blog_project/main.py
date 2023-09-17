58f907317ed6b614b796b47c310b4e54a94e68ca
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-17 10:33:05 +0200

    blog project getting posts from API in JSON

--- udemy/day_57/2_blog_project/main.py
+++ udemy/day_57/2_blog_project/main.py
@@ -1,11 +1,39 @@
+from post import Post
+import requests
 from flask import Flask, render_template
 
+BLOG_URL = "https://api.npoint.io/c790b4d5cab58020d391"
 
 app = Flask(__name__)
 
+all_posts = []
+response = requests.get(BLOG_URL)
+response.raise_for_status()
+
+json_data = response.json()
+
+for post in json_data:
+    p = Post(
+        post_id=int(post["id"]),
+        title=str(post["title"]),
+        subtitle=post["subtitle"],
+        body=post["body"])
+    all_posts.append(p)
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_blogs=all_posts)
+
+@app.route("/post/<int:blog_id>")
+def show_blog(blog_id):
+    blog = None
+    for loop_post in all_posts:
+        if loop_post.id[0] == blog_id:
+            blog = loop_post
+    return render_template("post.html", blog=blog)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+

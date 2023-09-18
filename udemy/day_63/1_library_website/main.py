5a696fff6e3652e5e36e5cccc46530828aee6023
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-18 18:54:34 +0200

    flask_sqlalchemy library create DB/table and CRUD operations

---
+++ udemy/day_63/1_library_website/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        book = {
+            "title": request.form.get("title"),
+            "author": request.form.get("author"),
+            "rating": request.form.get("rating"),
+        }
+        all_books.append(book)
+        print(all_books)
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+

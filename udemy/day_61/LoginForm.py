7a02de997ad164facdb89b1637fca6b43a9e4884
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-18 10:00:16 +0200

    Login form implementation and verification

---
+++ udemy/day_61/LoginForm.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+"""
+SPECIFICATIONS:
+
+It must have an email and password field
+They can both be StringFields.
+You don't have to worry about validators.
+Both email and password inputs should be size 30. (This describes the width of the input).
+You should not need to create any <label> or <input> elements manually using HTML.
+"""
+

267453aae4b3a84a586e96b818bb4b91b7610daf
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-18 20:48:12 +0200

    add books to databasse

--- udemy/day_63/1_library_website/main.py
+++ udemy/day_63/1_library_website/main.py
@@ -1,25 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+db = SQLAlchemy()
 
-all_books = []
+# Create a New Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+db.init_app(app)
 
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+def convert_scalar_to_ojb(scalars):
+    all_books = []
+    for s in scalars:
+        obj = {
+            "title": s.title,
+            "author": s.author,
+            "rating": s.rating
+        }
+        all_books.append(obj)
+    return all_books
 
 @app.route('/')
 def home():
+    with app.app_context():
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = convert_scalar_to_ojb(result.scalars())
+        print(all_books)
+
     return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        book = {
-            "title": request.form.get("title"),
-            "author": request.form.get("author"),
-            "rating": request.form.get("rating"),
-        }
-        all_books.append(book)
-        print(all_books)
+        with app.app_context():
+            book = Book(title=request.form.get("title"),
+                        author=request.form.get("author"),
+                        rating=request.form.get("rating"))
+            db.session.add(book)
+            db.session.commit()
+        return redirect(url_for('home'))
     return render_template('add.html')
 
 

0e8e837b2c0f12041e7b95a730f1eadcc9d7c80b
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-19 11:13:57 +0200

    Added two movies into DB

--- udemy/day_64/main.py
+++ udemy/day_64/main.py
@@ -20,8 +20,52 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+db = SQLAlchemy()
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(450), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(300), nullable=False)
+    img_url = db.Column(db.String(300), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# with app.app_context():
+#     first_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#     db.session.add(first_movie)
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 
 @app.route("/")
@@ -30,4 +74,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)

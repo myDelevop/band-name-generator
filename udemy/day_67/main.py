6003f858ad62d57507c2ab81c7f3cfad7119d027
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-20 12:07:53 +0200

    logic for delete a post

--- udemy/day_67/main.py
+++ udemy/day_67/main.py
@@ -87,7 +87,6 @@ def add_new_post():
         return render_template("make-post.html", form=form)
 
 
-
 @app.route("/edit-post/<post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
@@ -109,7 +108,14 @@ def edit_post(post_id):
 
     return render_template("make-post.html", form=edit_form, id=post_id, is_edit=True)
 
-# TODO: delete_post() to remove a blog post from the database
+
+@app.route("/delete_post/<post_id>")
+def delete_post(post_id):
+    post = BlogPost.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")

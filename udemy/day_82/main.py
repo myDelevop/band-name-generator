d31fc88ac096dbb1867ca3382351cef14de23834
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-24 20:30:12 +0200

    Added simple space in input function

--- udemy/day_82/main.py
+++ udemy/day_82/main.py
@@ -1,6 +1,6 @@
 from morse_code import MorseCode
 
-sentence = input("Please insert the sentence you want to convert in Morse Code")
+sentence = input("Please insert the sentence you want to convert in Morse Code: ")
 
 mc = MorseCode()
 print(mc.encode_sentence(sentence))

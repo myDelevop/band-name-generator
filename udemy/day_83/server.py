1f9d24abc4e99ea96fa253d646c4083e621b6789
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-25 09:59:25 +0200

    Portfolio home page

---
+++ udemy/day_83/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return "About"
+
+
+@app.route('/contact')
+def contact():
+    return "Contact"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
